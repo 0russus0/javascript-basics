ea70b1bdcd884356b5f4f9dc68643512746f1a2a
0russus0 <pierreetleroux@gmail.com>
2023-09-25 11:55:52 +0200

    part 3 & 4

--- 08 - functions-basics/js/exercice3.js
+++ 08 - functions-basics/js/exercice3.js
@@ -1,1 +1,15 @@
 // Your code goes here
+//ajouterPropriete
+let user = {
+	name: 'Big',
+	firstName: 'Jim',
+	age: 50,
+};
+console.log(user + 'toto');
+user.job = 'Ninja';
+user.none = 'A supprimer';
+console.log(user);
+
+//supprimerPropriete
+delete user.none;
+console.log(user);
--- 08 - functions-basics/js/exercice4.js
+++ 08 - functions-basics/js/exercice4.js
@@ -1,1 +1,27 @@
 // Your code goes here
+// Exercice 4 : Gestion des Dates
+function afficherDate(date) {
+	date = new Date();
+	let dateLocale = date.toLocaleString('fr-FR', {
+		day: 'numeric',
+		month: 'numeric',
+		year: 'numeric',
+		hour: 'numeric',
+		minute: 'numeric',
+		second: 'numeric',
+	});
+	return dateLocale;
+}
+today = afficherDate();
+console.log(today);
+
+function calculerAge(birthYear, date) {
+	birthYear = 1978;
+	date = new Date();
+	let year = date.getFullYear();
+	let age = year - birthYear;
+	return age;
+}
+
+old = calculerAge();
+console.log(old);
