7e36b9d7091fe83d19e90a5e23ee969eb4604495
0russus0 <pierreetleroux@gmail.com>
2023-09-25 15:42:29 +0200

    end of 3 of part 09

--- 09 - while/js/exercice3.js
+++ 09 - while/js/exercice3.js
@@ -1,16 +1,16 @@
 // Your code goes here
 //Recherche d'un Élément
 let elements = ['caratères', 1935, '1955', 2, 'coucou'];
-console.log(elements);
-let elementRecherche = 'coucou';
-console.log(elementRecherche + ' test');
-elm = elements[i];
-console.log(elm);
-let cmpt = 0;
 let arrayLength = elements.length;
+let elementRecherche = 'coucou';
+elm = elements[0];
+
+let cmpt = 0;
+
 while (cmpt < arrayLength) {
-	if (elm === elementRecherche[i]) {
+	if (elements.includes(elementRecherche)) {
 		console.log("L'élément recherché est bien " + elementRecherche);
+		break;
 	} else {
 		cmpt++;
 	}
