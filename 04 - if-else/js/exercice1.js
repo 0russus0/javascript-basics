17a9579117f4896b65b48088a6f0f049be250273
0russus0 <pierreetleroux@gmail.com>
2023-09-25 10:32:09 +0200

    commit for end of first part of 3 and 4

--- 04 - if-else/js/exercice1.js
+++ 04 - if-else/js/exercice1.js
@@ -1,1 +1,13 @@
 // Your code goes here
+// # Exercices JavaScript : Comparaisons
+let nombre1 = 5;
+let nombre2 = 3;
+if (nombre1 == nombre2) {
+	console.log(nombre1 + 'est égal à ' + nombre2);
+} else if (nombre1 != nombre2) {
+	console.log(nombre1 + "n'est pas égal à " + nombre2);
+} else if (nombre1 > nombre2) {
+	console.log(nombre1 + 'est plus grand que ' + nombre2);
+} else {
+	console.log(nombre1 + 'est plus petit que ' + nombre2);
+}
